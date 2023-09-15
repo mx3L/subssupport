315813dcfe01112a52ef82d8c1035adbbf34425e
biko-73 <57875905+biko-73@users.noreply.github.com>
2023-09-15 20:28:35 +0300

    Update __init__.py

--- plugin/seekers/__init__.py
+++ plugin/seekers/__init__.py
@@ -1,3 +1,16 @@
+'''
+Created on Feb 10, 2014
+
+@author: marko
+'''
 from __future__ import absolute_import
-# Dummy file to make this directory a package.
-from . import service as subtitlesmora
+try:
+    from . import _
+except ImportError:
+    def _(txt):             
+        return txt
+
+from .seeker import SubtitlesDownloadError, SubtitlesSearchError, SubtitlesErrors
+from .xbmc_subtitles import TitulkyComSeeker, EdnaSeeker, SerialZoneSeeker, ElsubtitleSeeker, IndexsubtitleSeeker, MoviesubtitlesSeeker, Moviesubtitles2Seeker, MySubsSeeker, \
+    OpenSubtitlesSeeker, OpenSubtitles2Seeker, PodnapisiSeeker, SubsceneSeeker, SubdlSeeker, SubsytsSeeker, SubtitlecatSeeker, SubtitlesGRSeeker, SubtitlesmoraSeeker, SubtitlistSeeker, \
+    ItasaSeeker, TitloviSeeker
