a2fafb4dc6cc291c57955b631df54c053d6a137b
biko-73 <57875905+biko-73@users.noreply.github.com>
2023-09-15 20:00:18 +0300

    Add files via upload

--- plugin/seekers/__init__.py
+++ plugin/seekers/__init__.py
@@ -1,16 +1,3 @@
-'''
-Created on Feb 10, 2014
-
-@author: marko
-'''
 from __future__ import absolute_import
-try:
-    from . import _
-except ImportError:
-    def _(txt):             
-        return txt
-
-from .seeker import SubtitlesDownloadError, SubtitlesSearchError, SubtitlesErrors
-from .xbmc_subtitles import TitulkyComSeeker, EdnaSeeker, SerialZoneSeeker, ElsubtitleSeeker, IndexsubtitleSeeker, MoviesubtitlesSeeker, Moviesubtitles2Seeker, MySubsSeeker, \
-    OpenSubtitlesSeeker, PodnapisiSeeker, SubsceneSeeker, SubdlSeeker, SubsytsSeeker, SubtitlecatSeeker, SubtitlesGRSeeker, SubtitlesmoraSeeker, SubtitlistSeeker, \
-    ItasaSeeker, TitloviSeeker
+# Dummy file to make this directory a package.
+from . import service as subtitlesmora
