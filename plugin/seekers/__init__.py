8018c8c7c2e34e4341f34690c580c167315baa02
biko-73 <57875905+biko-73@users.noreply.github.com>
2023-12-05 10:21:32 +0200

    mino60 update (#89)
    Change mora21 Subtitle folder link from archive.org/download/mora2023 to New one archive.org/download/mora25r

--- plugin/seekers/__init__.py
+++ plugin/seekers/__init__.py
@@ -12,5 +12,5 @@ except ImportError:
 
 from .seeker import SubtitlesDownloadError, SubtitlesSearchError, SubtitlesErrors
 from .xbmc_subtitles import TitulkyComSeeker, EdnaSeeker, SerialZoneSeeker, ElsubtitleSeeker, IndexsubtitleSeeker, MoviesubtitlesSeeker, Moviesubtitles2Seeker, MySubsSeeker, \
-    OpenSubtitlesSeeker, PodnapisiSeeker, SubsceneSeeker, SubdlSeeker, SubsytsSeeker, SubtitlecatSeeker, SubtitlesGRSeeker, SubtitlesmoraSeeker, SubtitlistSeeker, \
+    OpenSubtitlesSeeker, OpenSubtitles2Seeker, PodnapisiSeeker, SubsceneSeeker, SubdlSeeker, SubsytsSeeker, SubtitlecatSeeker, SubtitlesGRSeeker, SubtitlesmoraSeeker, SubtitlistSeeker, \
     ItasaSeeker, TitloviSeeker
